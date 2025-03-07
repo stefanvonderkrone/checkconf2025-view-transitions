cc95e367559415a6af17638d3d86ff497fb7423c
Stefan von der Krone <stefan.krone@check24.de>
2025-03-07 09:54:27 +0100

    move everything to App.tsx.

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,88 @@
-import AppVsWeb from './slides/AppVsWeb';
-import InitialExamples from './slides/InitialExamples';
-import Intro from './slides/Intro';
-import PreviousSolutions from './slides/PreviousSolutions';
-import ViewTransitionBasics from './slides/ViewTransitionBasics';
+import Slide from './Slide';
 import SlidesRouter from './SlidesRouter';
+import KeyVisual from './assets/key_visual.gif';
+import LevelUp from './assets/level_up.svg';
 
 const SLIDES = [
-    <Intro />,
-    <AppVsWeb />,
-    <PreviousSolutions />,
-    <InitialExamples />,
-    <ViewTransitionBasics />,
+    <Slide
+        background={
+            <picture className="block h-full w-full">
+                <img
+                    alt="key visual"
+                    className="h-full w-full"
+                    src={KeyVisual}
+                />
+            </picture>
+        }
+    >
+        <picture>
+            <img alt="level up" src={LevelUp} className="w-[36.25rem]" />
+        </picture>
+        <h1>Stop the Flash, Start the Flow</h1>
+        <hr className="w-[57.5rem] border-[0.125rem] border-white" />
+        <h3>Leveling Up with View Transitions</h3>
+        <h3>Stefan von der Krone | 28/06/24 | 14:00 Uhr</h3>
+    </Slide>,
+
+    <Slide
+        invertLogo
+        background={
+            <div className="flex h-full w-full flex-row">
+                <div className="h-full flex-1 bg-[#063773]" />
+                <div className="h-full flex-1 bg-white" />
+            </div>
+        }
+    >
+        <h1>App vs. Web</h1>
+    </Slide>,
+
+    <Slide invertLogo>
+        <h1>Previous Solutions</h1>
+        <ul>
+            <li>React Transition Group</li>
+            <li>GSAP</li>
+            <li>Framer Motion</li>
+        </ul>
+    </Slide>,
+
+    <Slide invertLogo>
+        <h1>Some examples</h1>
+        <ul>
+            <li>Simple</li>
+            <li>Slide In</li>
+            <li>Slide To Key Visual</li>
+        </ul>
+    </Slide>,
+
+    <Slide invertLogo>
+        <h1>View Transition Basics</h1>
+        <ul>
+            <li>Focus On Same Document View Transitions</li>
+        </ul>
+        <pre>
+            <code>
+                {`
+const transition = document.startViewTransition(() => {
+    updateTheDom();
+})
+`}
+            </code>
+        </pre>
+        <pre>
+            <code>
+                {`
+::view-transition,
+::view-transition-group(root),
+::view-transition-image-pair(root),
+::view-transition-old(root),
+::view-transition-new(root) {
+    /* define animation here */
+}
+`}
+            </code>
+        </pre>
+    </Slide>,
+
     <h1 className="text-white">Slide 7</h1>,
 ];
 
